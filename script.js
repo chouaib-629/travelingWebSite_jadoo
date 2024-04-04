efb2b153a6b14b5dc07272818493c49c8d20de6e
chouaib <chouaiba629@gmail.com>
2024-04-04 13:38:58 +0100

    Template for traveling website

---
+++ script.js
@@ -0,0 +1,15 @@
+// sticky header
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+});
+
+// responsive navigation menu
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector("nav");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navmenu.classList.toggle("open");
+};
